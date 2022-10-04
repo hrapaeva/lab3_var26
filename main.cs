39c923d1d73ec397dc7d3b2bdcf06af24554f9f7
hrapaeva <104451032+hrapaeva@users.noreply.github.com>
2022-10-04 15:24:26 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,24 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+      Console.WriteLine("Введите x: ");
+      double x = Convert.ToDouble(Console.ReadLine());
+      Console.WriteLine("Введите y: ");
+      double y = Convert.ToDouble(Console.ReadLine());
+
+      if ((Math.Abs(x+y) + Math.Abs(x-y) <= 12) && (x*x + y*y <= 36)) {
+      if (y >= -2*x + 6) 
+        Console.WriteLine($"Точка находится в закрашенной области"); 
+      else if ((y >= 2*x + 6) && (y >= -2*x - 6))  
+        Console.WriteLine($"Точка находится в закрашенной области"); 
+      else if ((x <= 0) && (y <= 0) && (y <= -2*x - 6)) 
+        Console.WriteLine($"Точка находится в закрашенной области"); 
+      else if ((x >= 0) && (y <= 0) && (y <= -2*x + 6) && (y <= 2*x - 6))
+        Console.WriteLine($"Точка находится в закрашенной области"); 
+      else 
+        Console.WriteLine($"Точка вне закрашенной области"); 
+      }
+      else  Console.WriteLine($"Точка вне данной области");
+ }
+}
